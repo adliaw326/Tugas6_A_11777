2ae536f4e8cad19aba52bd6f3f5506f32b321b21
adliaw326 <220711777@students.uajy.ac.id>
2024-11-28 15:31:39 +0700

    Update Tugas_Modul6_A_ADLI_11777.py

--- Tugas_Modul6_A_ADLI_11777.py
+++ Tugas_Modul6_A_ADLI_11777.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-model = load_model(r'D:\Matakuliah Semester 5\Pembelajaran Mesin dan Pembelajaran Mendalam\CNN\Tugas6_A_11777\model_googlenet.h5')
+model = load_model('model_googlenet.h5')
 class_names = ['Matang', 'Mentah']
 
 def classify_image(image_path):
